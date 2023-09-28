7e013026e466c2dbaef442f209a25aac74477be7
kkaps002 <kapsimaliscostas@gmail.com>
2023-09-28 03:27:36 +0100

    added button to change number of boxes on the grid and made the grid adjust its height and width dynamically

--- javascript.js
+++ javascript.js
@@ -1,48 +1,58 @@
 
 // create column and row divs and fill those with 16 blocks , flex direction to set direction 
 const gridContainer = document.querySelector(".grid-container")
-
-gridContainer.style.height = "100vh";
-gridContainer.style.width = "100%"
-gridContainer.style.display = "flex";
-gridContainer.style.gap = "0";
-// gridContainer.style.flexWrap = "wrap";
+const numbtn = document.querySelector("#numbtn");
 
 
-for (let j =0; j < 16 ; j++) {  // create a column container 
+let numofboxes = 16; // init with default value
+makeBoxes(); //generate initial 16 box grid 
 
-    const columnContainer = document.createElement('div');
-    columnContainer.textContent = "column container"; // fix!!!!! this sets the min width 
-    columnContainer.style.display = "flex";
-    columnContainer.style.flexFlow = "column wrap";
-    columnContainer.style.minHeight = '0px';
-    columnContainer.style.margin = "0px";
-    gridContainer.appendChild(columnContainer);
-
-    //fill an individual column container with blocks
-    for (let i = 0; i < 16; i++) { 
-        // Create a new <div> element
-        const newBlock = document.createElement('div');
-    
-        // for debugging 
-        //newBlock.textContent = `block ${i + 1}`;
-    
-        // Append the new <div> to a container element 
-        columnContainer.appendChild(newBlock);
-        
-        
-        newBlock.style.flexGrow = "1";
-        newBlock.style.justifyContent = "center";
-        newBlock.style.alignItems = "center";
-        newBlock.style.border = "1px solid black";
-        newBlock.classList.add("block"); // Add the "block" class to created divs for css styling 
-
-
-        newBlock.addEventListener("click", function(){
-            newBlock.style.background = "grey";
-        })
+numbtn.addEventListener("click", function (){ //click checks what the number of boxes is and behaves accordingly
+    if(numofboxes === 64){ // maximum number of boxes
+        gridContainer.innerHTML = ""; // clear grid
+        numofboxes = 16; // reset to 16 
+        makeBoxes();
     }
+    else{
+        gridContainer.innerHTML = ""; // clear grid
+        numofboxes *= 2; // duplicate num of boxes to make 
+        makeBoxes(); // and create 
+    }
+}); 
 
+function makeBoxes() { // 
+    for (let j =0; j < numofboxes ; j++) {  // create a column container 
+
+        const columnContainer = document.createElement('div');
+        columnContainer.style.display = "flex";
+        columnContainer.style.flex = "1";
+        columnContainer.style.flexFlow = "column wrap";
+        columnContainer.style.margin = "0px";
+        gridContainer.appendChild(columnContainer);
+    
+        //fill an individual column container with blocks
+        for (let i = 0; i < numofboxes; i++) { 
+            // Create a new <div> element
+            const newBlock = document.createElement('div');
+        
+            // Append the new <div> to a container element 
+            columnContainer.appendChild(newBlock);
+            
+            
+            newBlock.style.border = "1px solid black";
+            newBlock.classList.add("block"); // Add the "block" class to created divs for css styling 
+    
+    
+            newBlock.addEventListener("mouseover", function(){
+                newBlock.style.background = "grey";
+            })
+        }
+    }
 }
+
+
+
+
+
 
 
